c2103f4e7d9f1584172567d61e553a5d7163fa12
li <lxx610103@163.com>
2020-05-20 21:57:26 +0800

    搜索界面添加recycleView

--- findMentorApp/app/src/main/java/com/example/findmentorapp/ui/search/SearchFragment.java
+++ findMentorApp/app/src/main/java/com/example/findmentorapp/ui/search/SearchFragment.java
@@ -11,12 +11,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.findmentorapp.R;
 
 public class SearchFragment extends Fragment {
 
     private SearchViewModel searchViewModel;
+
+    String s_tilte[] = {"内容一", "内容一", "内容一"};
+    String s_text[] = {"内容二", "内容二", "内容二"};
+    String s_name[] = {"内容", "内容", "内容"};
+    String s_time[] = {"内容1", "内容1", "内容1"};
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +38,67 @@ public class SearchFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        final RecyclerView recyclerView_search = root.findViewById(R.id.recyclerView_search_forSerach);
+        recyclerView_search.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+
+        MyAdapter myAdapter = new MyAdapter();
+
+        recyclerView_search.setAdapter(myAdapter);
+
+
+
         return root;
     }
+
+    class MyAdapter extends RecyclerView.Adapter{
+
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            MyHolder myHolder = new MyHolder(LayoutInflater.from(getActivity()).inflate(R.layout.item_recyclerview_forsearch, null));
+            return myHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+            MyHolder mm = (MyHolder) holder;
+
+            //将数据映射到控件中
+            mm.title.setText(s_tilte[position]);
+            mm.text.setText(s_text[position]);
+            mm.name.setText(s_name[position]);
+            mm.time.setText(s_time[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return s_tilte.length;
+        }
+
+        class MyHolder extends RecyclerView.ViewHolder {
+
+            TextView title, text, name, time;
+
+            public MyHolder(View itemView) {
+                super(itemView);
+
+                //实例化子对象，把对象和列表项布局文件中的id绑定
+                title = itemView.findViewById(R.id.textView_item_forSearch_title);
+                text = itemView.findViewById(R.id.textView_item_forSearch_text);
+                name = itemView.findViewById(R.id.textView_item_forSearch_name);
+                time = itemView.findViewById(R.id.textView_item_forSearch_time);
+
+            }
+
+        }
+    }
+
+
+
+
+
+
 }
