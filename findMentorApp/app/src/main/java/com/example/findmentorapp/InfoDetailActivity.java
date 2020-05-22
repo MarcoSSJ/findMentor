26017f33661612d262c34d741daf96c65b79e6c9
li <lxx610103@163.com>
2020-05-22 22:54:13 +0800

    搜索界面完善

---
+++ findMentorApp/app/src/main/java/com/example/findmentorapp/InfoDetailActivity.java
@@ -0,0 +1,56 @@
+package com.example.findmentorapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InfoDetailActivity extends AppCompatActivity{
+
+    private TextView textView_title;
+    private TextView textView_name;
+    private TextView textView_personInfo;
+    private TextView textView_detailInfo;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infodetail);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        //各种信息
+        //todo 本页面暂时先不管等活动能提交了再处理
+        textView_title = (TextView)findViewById(R.id.textView_infoDetail_title);
+        textView_name = (TextView)findViewById(R.id.textView_infoDetail_name);
+        textView_personInfo = (TextView)findViewById(R.id.textView_infoDetail_personInfo);
+        textView_detailInfo = (TextView)findViewById(R.id.textView_infoDetail_detailInfo);
+
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
--- findMentorApp/app/src/main/java/com/example/findmentorapp/ui/search/SearchFragment.java
+++ findMentorApp/app/src/main/java/com/example/findmentorapp/ui/search/SearchFragment.java
@@ -1,9 +1,13 @@
 package com.example.findmentorapp.ui.search;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +18,9 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.findmentorapp.AccountActivity;
+import com.example.findmentorapp.InfoDetailActivity;
+import com.example.findmentorapp.MainActivity;
 import com.example.findmentorapp.R;
 
 public class SearchFragment extends Fragment {
@@ -39,6 +46,19 @@ public class SearchFragment extends Fragment {
             }
         });
 
+        //搜索输入框和搜索按钮
+        final EditText searchEditText = root.findViewById(R.id.editText_search_searchByWord);
+
+        final Button searchButton = root.findViewById(R.id.button_search_searchByText);
+        searchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v) {
+                //Todo 点击搜索按钮后操作
+                String searchText = searchEditText.getText().toString();
+            }
+        });
+
+        //RecyclerView相关函数，暂时放在下面。以后可能需要放入ViewModel
         final RecyclerView recyclerView_search = root.findViewById(R.id.recyclerView_search_forSerach);
         recyclerView_search.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
 
@@ -53,24 +73,38 @@ public class SearchFragment extends Fragment {
 
     class MyAdapter extends RecyclerView.Adapter{
 
+        private Context mcontent;
+
 
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
             MyHolder myHolder = new MyHolder(LayoutInflater.from(getActivity()).inflate(R.layout.item_recyclerview_forsearch, null));
+
+
             return myHolder;
         }
 
         @Override
         public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
-            MyHolder mm = (MyHolder) holder;
+            MyHolder viewHolder = (MyHolder) holder;
 
             //将数据映射到控件中
-            mm.title.setText(s_tilte[position]);
-            mm.text.setText(s_text[position]);
-            mm.name.setText(s_name[position]);
-            mm.time.setText(s_time[position]);
+            viewHolder.title.setText(s_tilte[position]);
+            viewHolder.text.setText(s_text[position]);
+            viewHolder.name.setText(s_name[position]);
+            viewHolder.time.setText(s_time[position]);
+
+            viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // todo 点击item事件,以后需要从这里跳到详情页，需要传去参数（position表示所在位置‘下面类中加了个id可以用来存id用于跳转）
+                    Intent intent = new Intent(getActivity(), InfoDetailActivity.class);
+                    startActivity(intent);
+                }
+            });
+
         }
 
         @Override
@@ -81,6 +115,8 @@ public class SearchFragment extends Fragment {
         class MyHolder extends RecyclerView.ViewHolder {
 
             TextView title, text, name, time;
+            //标识号
+            int id;
 
             public MyHolder(View itemView) {
                 super(itemView);
