4af27a2448e8bf339e1c72d934b87398b6c52fe7
li <lxx610103@163.com>
2020-05-23 21:50:18 +0800

    提交页面文字部分初步完成

--- findMentorApp/app/src/main/java/com/example/findmentorapp/ui/add/AddFragment.java
+++ findMentorApp/app/src/main/java/com/example/findmentorapp/ui/add/AddFragment.java
@@ -1,9 +1,12 @@
 package com.example.findmentorapp.ui.add;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.findmentorapp.LoginActivity;
 import com.example.findmentorapp.R;
 
 public class AddFragment extends Fragment {
@@ -30,6 +34,21 @@ public class AddFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        //标题与信息输入
+        final EditText editText_title = root.findViewById(R.id.editText_add_title);
+        final EditText editText_info = root.findViewById(R.id.editText_add_info);
+
+        Button button_submit = (Button)root.findViewById(R.id.button_add_submit);
+        button_submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v) {
+            //todo 点击提交按钮后处理，输入框在上面，注意多行输入
+
+            }
+        });
+
+
         return root;
     }
 }
