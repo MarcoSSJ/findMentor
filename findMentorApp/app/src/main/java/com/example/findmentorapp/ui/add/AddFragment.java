4ce8f98ada330ba381d1fabd75c4d799070c1b51
MarcoSSJ <sunsj16@mails.tsinghua.edu.cn>
2020-06-17 14:30:03 +0800

    小修改

--- findMentorApp/app/src/main/java/com/example/findmentorapp/ui/add/AddFragment.java
+++ findMentorApp/app/src/main/java/com/example/findmentorapp/ui/add/AddFragment.java
@@ -2,12 +2,16 @@ package com.example.findmentorapp.ui.add;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,10 +22,22 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.findmentorapp.LoginActivity;
 import com.example.findmentorapp.MyApplication;
 import com.example.findmentorapp.R;
+import com.example.findmentorapp.RegisterActivity;
+import com.example.findmentorapp.Urls;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class AddFragment extends Fragment {
 
-    //todo 将此作为类似动态发布的界面
     private AddViewModel addViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -70,14 +86,105 @@ public class AddFragment extends Fragment {
             textView2.setVisibility(View.VISIBLE);
         }
 
+
         button_submit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View v) {
-                //todo 点击提交按钮后处理，输入框在上面，注意多行输入
-
+                new Thread(runnable).start();
             }
         });
 
         return root;
     }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            String api_url = Urls.api_url;
+
+            Handler handler = new Handler(Looper.getMainLooper()) {
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    if (msg.what == 0) {
+                        //弹窗
+                        Toast toast = Toast.makeText(MyApplication.getContext(), "发布失败", Toast.LENGTH_SHORT);
+                        toast.show();
+                    } else if (msg.what == 1) {
+                        Toast toast = Toast.makeText(MyApplication.getContext(), "发布成功", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+            };
+
+
+            try {
+                URL url = new URL(api_url);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setReadTimeout(5000);
+                conn.setConnectTimeout(5000);
+
+                conn.setRequestProperty("Content-Type",
+                        "application/x-www-form-urlencoded;charset=UTF-8");
+
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+                conn.setUseCaches(false);
+
+                String data = "action=add"+"&type=" + URLEncoder.encode("teacher","UTF-8");
+
+                OutputStream out = conn.getOutputStream();
+                out.write(data.getBytes());
+                out.flush();
+                out.close();
+
+                InputStream is = conn.getInputStream();
+                if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    StringBuilder response = new StringBuilder();
+                    byte[] b = new byte[1024];
+                    int len;
+                    while ((len = is.read(b)) != -1) {
+                        response.append(new String(b, 0, len));
+                    }
+                    is.close();
+                    conn.disconnect();
+
+                    String res = new String(response);
+                    System.out.println(res);
+                    JSONObject obj = new JSONObject(res);
+                    String login = obj.getString("result");
+                    if (login.equals("true")) {
+                        Message message = Message.obtain();
+                        message.what = 1;
+                        handler.sendMessage(message);
+                    } else {
+                        Message message = Message.obtain();
+                        message.what = 0;
+                        handler.sendMessage(message);
+                    }
+                } else {
+                    Message message = Message.obtain();
+                    message.what = 0;
+                    handler.sendMessage(message);
+                }
+            } catch (MalformedURLException e) {
+                Message message = Message.obtain();
+                message.what = 0;
+                handler.sendMessage(message);
+                e.printStackTrace();
+            } catch (IOException e) {
+                Message message = Message.obtain();
+                message.what = 0;
+                handler.sendMessage(message);
+                e.printStackTrace();
+            } catch (JSONException e) {
+                Message message = Message.obtain();
+                message.what = 0;
+                handler.sendMessage(message);
+                e.printStackTrace();
+            }
+        }
+
+    };
 }
