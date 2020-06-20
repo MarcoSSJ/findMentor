51910c6bb7baca1e99aba65bec10515ad6e27840
MarcoSSJ <sunsj16@mails.tsinghua.edu.cn>
2020-06-21 00:13:35 +0800

    头像

--- findMentorApp/app/src/main/java/com/example/findmentorapp/ui/message/MessageFragment.java
+++ findMentorApp/app/src/main/java/com/example/findmentorapp/ui/message/MessageFragment.java
@@ -2,12 +2,15 @@ package com.example.findmentorapp.ui.message;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -144,7 +147,7 @@ public class MessageFragment extends Fragment {
         }
 
         @Override
-        public void onBindViewHolder(MyHolder holder, int position) {
+        public void onBindViewHolder(MyHolder holder, final int position) {
 
             final MyAdapter.MyHolder viewHolder = (MyAdapter.MyHolder) holder;
 
@@ -154,8 +157,103 @@ public class MessageFragment extends Fragment {
             viewHolder.time.setText(mFilterList.get(position).time);
             //用户id
             viewHolder.id = mFilterList.get(position).id;
-            //todo 这里加头像，跟搜索应该一样
-            //viewHolder.img.setImageBitmap(bitmap);
+            Runnable runnable1 = new Runnable() {
+                Bitmap bitmap = null;
+                @Override
+                public void run() {
+                    String personal_data_change_url = Urls.api_url;
+                    Handler handler = new Handler(Looper.getMainLooper()) {
+                        @Override
+                        public void handleMessage(Message msg) {
+                            super.handleMessage(msg);
+                            if (msg.what == 0) {
+
+                            } else if (msg.what == 1) {
+                                //成功
+                                viewHolder.img.setImageBitmap(bitmap);
+                            }
+                        }
+                    };
+                    try {
+                        URL url = new URL(personal_data_change_url);
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setChunkedStreamingMode(0);
+                        conn.setRequestMethod("POST");
+                        conn.setReadTimeout(5000);
+                        conn.setConnectTimeout(5000);
+
+                        conn.setRequestProperty("Content-Type",
+                                "application/x-www-form-urlencoded;charset=UTF-8");
+
+                        conn.setDoOutput(true);
+                        conn.setDoInput(true);
+                        conn.setUseCaches(false);
+
+                        //MyApplication application = (MyApplication) getActivity().getApplicationContext();
+                        MyApplication application = MyApplication.getInstance();
+                        String sessionID = application.getSessionID();
+
+                        String data = "action=DownloadPic" +
+                                "&sessionID=" + URLEncoder.encode(sessionID, "UTF-8")+
+                                "&id=" + URLEncoder.encode(mFilterList.get(position).id, "UTF-8");
+                        OutputStream out = conn.getOutputStream();
+                        out.write(data.getBytes());
+                        out.flush();
+                        out.close();
+
+                        InputStream is = conn.getInputStream();
+                        if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                            StringBuilder response = new StringBuilder();
+                            byte[] b = new byte[1024];
+                            int len;
+                            while ((len = is.read(b)) != -1) {
+                                response.append(new String(b, 0, len));
+                            }
+                            is.close();
+                            conn.disconnect();
+
+                            String res = new String(response);
+                            System.out.println(res);
+                            JSONObject obj = new JSONObject(res);
+                            String isconnect = obj.getString("result");
+                            if (isconnect.equals("true")) {
+
+                                String imgStr = obj.getString("picture");
+                                byte[] bytes = Base64.decode(imgStr, Base64.DEFAULT);
+                                bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                //s_pic.add(bitmap);
+                                Message message = Message.obtain();
+                                message.what = 1;
+                                handler.sendMessage(message);
+                            }else {
+                                Message message = Message.obtain();
+                                message.what = 0;
+                                handler.sendMessage(message);
+                            }
+                        } else {
+                            Message message = Message.obtain();
+                            message.what = 0;
+                            handler.sendMessage(message);
+                        }
+                    } catch (MalformedURLException e) {
+                        Message message = Message.obtain();
+                        message.what = 0;
+                        handler.sendMessage(message);
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        Message message = Message.obtain();
+                        message.what = 0;
+                        handler.sendMessage(message);
+                        e.printStackTrace();
+                    } catch (JSONException e) {
+                        Message message = Message.obtain();
+                        message.what = 0;
+                        handler.sendMessage(message);
+                        e.printStackTrace();
+                    }
+                }
+            };
+            new Thread(runnable1).start();
 
             viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
